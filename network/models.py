3bf88cec0a281a86cd2bcd33f6f63d5149c8bdfd
Admin <englishpoppop@gmail.com>
2024-01-16 12:09:41 -0500

    modified css and created post box

--- network/models.py
+++ network/models.py
@@ -4,3 +4,14 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+
+class Post(models.Model):
+    content=models.CharField(max_length=140)
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="author")
+    date=models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"post {self.id} made by {self.user} on {self.date.strftime('%d %b %Y %H:%M:%S')}"
